c8a1a92c5e83ef2caba6df65290762a4ffbc7629
Kilerd Chan <blove694@gmail.com>
2024-07-18 00:08:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
+  "docker-proxy.kilerd.me": dockerHub,
   "quay.libcuda.so": "https://quay.io",
   "gcr.libcuda.so": "https://gcr.io",
   "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
   "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
+  "docker-proxy-ghcr.kilerd.me": "https://ghcr.io",
   "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
   "ecr.libcuda.so": "https://public.ecr.aws",
 
